5249cc2fdf2985b2fc3a2095a43e859c1155e058
Ali Ramazanov <aliasifzade@gmail.com>
2023-09-02 19:20:07 +0400

    Added Password for DB

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const uri =
-  "mongodb+srv://aliramazanov:password@bookstore-mern.jaguboy.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://aliramazanov:4Xg7ntEeIc8SZiN9@bookstore-mern.jaguboy.mongodb.net/?retryWrites=true&w=majority";
