c4644fcb61973bf246be4e33d45468cdffa1f488
Ali Ramazanov <aliasifzade@gmail.com>
2023-09-02 19:16:01 +0400

    Project Initialized

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const uri =
+  "mongodb+srv://aliramazanov:password@bookstore-mern.jaguboy.mongodb.net/?retryWrites=true&w=majority";
---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { PORT, uri } from "./config.js";
+import booksRoute from "./routes/booksRoute.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5555",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(200).send("Welcome!");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Application successfully connected to the DataBase");
+    app.listen(PORT, () => {
+      console.log(`Application is listening to Port:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  });
